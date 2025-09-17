f6e4051fa432308267eb467222122b0e41346bf8
rayhaanroushan <rayhaanisworking@gmail.com>
2025-09-18 02:16:16 +0530

    today is the day 4 of clanguage

--- day 3/1.c
+++ day 3/1.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    printf("AURA++++");
+    long int ticketId = 458921;
+    char passenger[] = "rayhaan roushan";
+    char from[] = "delhi";
+    char to[] = "Hyderabad";
+    int seat number = 22;
+    float flare flight = 5500.75;
+    char class = CSE-B Engineering
+    printf("my ticketId number is %ld my name is name is %s iam present at %s state and going %s state and my seat number %d and the flare flight is %f and my class section name is %s",
+    ticketId,passenger,from,to,seat number,flare flight,class);
     return 0;
 }
---
+++ day 3/2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    char name[]                 = "rayhaan roushan";
+    char city []                = "Hyderabad";
+    char college[]              = "Nawab Shah Alam Khan College";
+    float grade                 = 8.8;
+    char bloodGroup             = 'B';
+    int age                     = 18;
+    long long int phonenumber  = 9701828235;
+printf("my name is %s iam living in %s state my college name is %s my 1oth grade is %f and my bloodGroup is %c my age is %d my phonenumber is %lld",
+  name,city,college,grade,bloodGroup,age,phonenumber);
+  return 0;
+
+}
