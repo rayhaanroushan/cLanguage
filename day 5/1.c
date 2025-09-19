de21f17999d52aba4f5b5061e866fbc88ed6ad79
rayhaanroushan <rayhaanisworking@gmail.com>
2025-09-20 01:05:44 +0530

    today was day 5 of cLangauge

---
+++ day 5/1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()[
+    char Name[]               = "Mohammed Rayhaan Roushan Ali";
+    char FatherName[]         = "Mohammed Akbar Ali";
+    char InterCollege[]       = "Narayana Junior College";
+    char EngineeringCollege[] = "Nawab Shah Alam Khan Engineering College";
+    int Age                   = 18;
+    char BloodGroup           = 'B';
+printf("My name is %s /n And my father name is %s /n My inter college was %s /n My Engineering college was %s /n My age is %d /n My blood group is %c",
+
+Name,FatherName,InterCollege,EngineeringCollege,Age,BloodGroup);
+return
+
+]
