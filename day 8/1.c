faec24dbc944cfc702dd44b955866fccd43445d4
rayhaanroushan <rayhaanisworking@gmail.com>
2025-09-24 23:43:24 +0530

    today is 8 day of learning c language

---
+++ day 8/1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    char Name      []= "Rayhaan Roushan";
+    char fatherName[]= "Akbar Ali";
+    int  age         = 18;
+    char bloodGroup = 'B';
+printf("My name is %s \n My father name is %s \n My age is %d \n My Blood Group is %c",Name,fatherName,age,bloodGroup);      
+return 0;
+}
---
+++ day 8/4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (){
+    int number  = 1000 + 1000;
+    int number2 = number -1000;
+    int number3 = 2000 + number2;
+    printf("%d \n",number);
+    printf("%d\n",number2);
+    printf("%d\n",number3);
+    return 0;
+}
