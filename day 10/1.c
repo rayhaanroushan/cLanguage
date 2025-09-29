22a67d423f21bcb91972fe75561c23e814a9dd6b
rayhaanroushan <rayhaanisworking@gmail.com>
2025-09-30 02:50:46 +0530

    day 10

---
+++ day 10/1.c
@@ -0,0 +1,11 @@
+Maareesan#include<stdio.h>
+int main (){
+    int a   = 150; 
+    int b   = 200;
+    int c   = 300;
+    int d   = 400;
+    int e   = 500;
+    int sum = a + b + c + d + e;
+    printf(" a value is = %d b value is = %d c value is = %d d value is = %d e value is = %d by totaling this a b c d and e we get = %d ",a,b,c,d,e,sum);
+    return 0;
+}
