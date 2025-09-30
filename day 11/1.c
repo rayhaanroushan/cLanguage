2abba89defa74ed61f4327a3337d78f853a613c9
rayhaanroushan <rayhaanisworking@gmail.com>
2025-10-01 00:33:40 +0530

    day 11

---
+++ day 11/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (){
+char Name[] = "Rayhaan Roushan";
+char fatherName[] = "Akbar ali";
+char collegeName[] ="Nawab Shah ALam Khan";
+int age = 18;
+char bloodGroup = 'B';
+long long int phoneNumber = 9701828235;
+printf("Hello my name is %s \n My father name is %s \n My college name is %s \n My age is %d \n My blood group is %c \n My phone Number is %lld",
+Name,fatherName,collegeName,age,bloodGroup,phoneNumber);
+return 0;
+}
---
+++ day 11/3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main (){
+ bool bTechIsVeryEasy         = true;
+ bool OneRupeeHasTenZeros     = false;
+ bool MacLaptopsAreCheapPrice = false;
+ bool GtaSixAreLaunched       = false;
+ bool GtaFiveIsOnlyFiveMb     = true;
+ printf("bTech is very easy%d \none rupee has ten zeros%d \nmac laptops are cheap price%d \ngta six are launched %d \n gta five is only five mb%d ",
+bTechIsVeryEasy,OneRupeeHasTenZeros,MacLaptopsAreCheapPrice,GtaSixAreLaunched,GtaFiveIsOnlyFiveMb);
+return 0;
+}
