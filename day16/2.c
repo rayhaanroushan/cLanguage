31d59d28bdc164df6202f05610b4fcce253a4505
rayhaanroushan <rayhaanisworking@gmail.com>
2025-10-06 02:45:02 +0530

    day 16

---
+++ day16/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void myFunction {
+  printf("Hello %s\n", name);
+}
+
+int main() {
+  myFunction("rayhaan:");
+  myFunction("suhail:");
+  myFunction("Muskaan:");
+  return 0;
+}
---
+++ day16/3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void myFunction(char name[], int age){
+  printf("Hello %s. You are %d years old\n", name, age);
+  main();
+  return 0;
+}
+
+int main() {
+  myFunction("rayhaan", 18);
+  myFunction("suhail", 23);
+  myFunction("shahrukh khan", 58);
+  main();
+  return 0;
+}
+
