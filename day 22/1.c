63e9656886139dd78f4aa950153bc8c2ad2dd987
rayhaanroushan <rayhaanisworking@gmail.com>
2025-10-14 00:57:35 +0530

    day 22 og C

---
+++ day 22/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void mybox(){
+    printf("rayhaan is smart\n");
+}
+
+int main(){
+    mybox();
+    mybox();
+    mybox();
+    mybox();
+    mybox();
+    mybox();
+    return 0;
+}
