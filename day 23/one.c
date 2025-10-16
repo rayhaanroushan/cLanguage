9cfb8a2a35e1fa08745b550370524574c4cbf2ef
rayhaanroushan <rayhaanisworking@gmail.com>
2025-10-16 23:25:23 +0530

    day 23 of C

---
+++ day 23/one.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (){
+char name [50];
+
+printf("enter your name sir:");
+scanf("%s",name);
+printf("Asalamalaikum %s\n",name);
+
+printf("How can i help you %s\n",name);
+return 0;
+}
